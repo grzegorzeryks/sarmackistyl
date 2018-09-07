80583979ce2c21b1c6c73aa16a97772a865f58e3
grzegorzeryks <grzegorzeryks@gmail.com>
2018-09-07 16:06:29 +0200

    smoothscroll

--- js/main.js
+++ js/main.js
@@ -25,4 +25,34 @@ crownLogo.addClass('crown-logo-move');
 //
 // });
 
+
+//Smooth scroll
+$('a[href*="#"]')
+
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    }
+  });
+
+
+
+
 });
