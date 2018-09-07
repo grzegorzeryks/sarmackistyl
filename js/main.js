c77722d4c68dd7ed1da96b8e386ff0b7a518a8b8
grzegorzeryks <grzegorzeryks@gmail.com>
2018-09-07 13:21:33 +0200

    background start section

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  console.log('ready!');
+let backLogo = $(".background-logo");
+console.log(backLogo);
+let graphic = $('#graphic');
+console.log(graphic);
+let crownLogo = $('.crown-logo');
+// back.on('click',function(){
+//   console.log('elo');
+//
+// });
+
+
+
+  backLogo.css('top', '11%');
+
+
+
+    crownLogo.addClass('crown-logo-move');
+
+  console.log('elooou');
+
+  console.log(crownLogo);
+
+
+});
